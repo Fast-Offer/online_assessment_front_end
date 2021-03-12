d756bbd10a159cb33927459ed19fe195c9d447e0
joeqiwx <56435623+joeqiwx@users.noreply.github.com>
2021-03-13 09:59:37 +1000

    delete the useless line
    some useless lines

--- craco.config.js
+++ craco.config.js
@@ -28,19 +28,4 @@ module.exports = {
       },
     },
   ],
-  // webpack: {
-  //   alias: {
-  //     '@': path.resolve(__dirname, 'src/')
-  //   }
-  // },
-  // webpack: {
-  //   configure: webpackConfig => {
-  //     const scopePluginIndex = webpackConfig.resolve.plugins.findIndex(
-  //       ({ constructor }) => constructor && constructor.name === 'ModuleScopePlugin'
-  //     );
-
-  //     webpackConfig.resolve.plugins.splice(scopePluginIndex, 1);
-  //     return webpackConfig;
-  //   }
-  // }
 };
