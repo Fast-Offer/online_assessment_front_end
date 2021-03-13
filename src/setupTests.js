fe39d6ceb14a450df9b64e3ce4a1948c72f92749
joeqiwx <56435623+joeqiwx@users.noreply.github.com>
2021-03-13 10:00:19 +1000

    Delete the useless lines

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,16 +1,4 @@
 import '@testing-library/jest-dom';
-
-// export default Object.defineProperty(global, 'matchMedia', {
-//   writable: true,
-//   value: jest.fn().mockImplementation(query => ({
-//     matches: false,
-//     media: query,
-//     onchange: null,
-//     addEventListener: jest.fn(),
-//     removeEventListener: jest.fn(),
-//     dispatchEvent: jest.fn(),
-//   })),
-// });
 global.matchMedia = global.matchMedia || function () {
   return {
     addListener: jest.fn(),
