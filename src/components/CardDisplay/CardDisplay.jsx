18c7b881e8777ced44d34e58d7ef219e91d9e243
jiahuiyang0@gmail.com <jiahuiyang0@gmail.com>
2021-03-23 17:04:25 +1100

    feat: 🎸 Library static page
    Static page of library
    
    ✅ Closes: OA-58

--- src/components/CardDisplay/CardDisplay.jsx
+++ src/components/CardDisplay/CardDisplay.jsx
@@ -1,27 +1,44 @@
 import React from 'react';
 // import ReactDOM from 'react-dom';
 import 'antd/dist/antd.css';
+import { Col, Row, Card } from 'antd';
 
 import contentStyle from './CardDisplay.module.less';
-import Cards from './Card';
+
+import data from './data'
+
+const { Meta } = Card;
+
+function cards(){
+  return(
+    <div className={contentStyle.card}>
+      <Row gutter={8}>
+       {data.map((card) => (
+         <Col className="gutter-row" span={6}>
+            <Card
+              hoverable
+                cover={<img alt="example" src= {card.url} 
+              />}
+            >
+              <Meta title= {card.title} description= {card.discription} />
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </div>
+  )
+};
 
 class Content extends React.Component {
   render() {
     return (
       <div className={contentStyle.background_container}>
         <div className={contentStyle.container}>
-          <Cards />
-          <Cards />
-          <Cards />
-          <Cards />
-          <Cards />
-          <Cards />
-          <Cards />
-          <Cards />
+          {cards()} 
         </div>
       </div>
     );
-  }
+  };
 }
 
 export default Content;
--- src/pages/Library/Library.jsx
+++ src/pages/Library/Library.jsx
@@ -1,79 +1,36 @@
 /* eslint-disable no-console */
 import React from 'react';
 import 'antd/dist/antd.css';
-// import './Layout.module.less';
+import { Breadcrumb } from 'antd';
 
-import {
-  Layout,
-  Menu,
-} from 'antd';
-
-import {
-  DesktopOutlined,
-  PieChartOutlined,
-  FileOutlined,
-  TeamOutlined,
-  UserOutlined,
-} from '@ant-design/icons';
-
+import Layout from 'antd/lib/layout/layout';
 import SearchBar from '../../components/SearchBar/SearchBar';
 import CardsDisplay from '../../components/CardDisplay/CardDisplay';
 import NavigationBar from '../../components/NavigationBar/NavigationBar';
+import Header from '../../components/Header/Header';
+import Footer from '../../components/Footer/Footer';
+import SideNavigation from '../../components/SideNavigation/SideNavigation';
 
-const {
-  Header,
-  Content,
-  Footer,
-  Sider,
-} = Layout;
-// const { SubMenu } = Menu;
+
 
 class LibraryLayout extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      collapsed: false,
-    };
-  }
-
-  // onCollapse = (collapsed) => {
-  //   console.log(collapsed);
-  //   this.setState({ collapsed });
-  // };
 
   render() {
-    const { collapsed } = this.state;
     return (
-      <Layout style={{ minHeight: '100vh' }}>
-        <Sider collapsible collapsed={collapsed} onCollapse={this.onCollapse}>
-          <div className="logo" />
-          <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-            <Menu.Item key="1" icon={<PieChartOutlined />}>
-              Library
-            </Menu.Item>
-            <Menu.Item key="2" icon={<DesktopOutlined />}>
-              Redo Quizs
-            </Menu.Item>
-            <Menu.Item key="3" icon={<UserOutlined />}>
-              Favorites
-            </Menu.Item>
-            <Menu.Item key="4" icon={<TeamOutlined />}>
-              My Result
-            </Menu.Item>
-            <Menu.Item key="5" icon={<FileOutlined />}>
-              Account
-            </Menu.Item>
-          </Menu>
-        </Sider>
-        <Layout className="site-layout">
-          <Header className="site-layout-background" style={{ padding: 0 }} />
-          <Content style={{ margin: '0 16px' }}>
+      <Layout>
+        <Header />
+        <Layout>
+          <SideNavigation />
+          <Layout>
+            <Breadcrumb style= {{ padding: '0 24px'}}>
+              <Breadcrumb.Item>Library</Breadcrumb.Item>
+            </Breadcrumb>
             <SearchBar />
             <NavigationBar />
             <CardsDisplay />
-          </Content>
-          <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+          </Layout>
         </Layout>
+        <Footer />
       </Layout>
     );
   }
