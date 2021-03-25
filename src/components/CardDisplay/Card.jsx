1a4a2985897c3be98c541fa31bcb6e7680dd58dc
jiahuiyang0@gmail.com <jiahuiyang0@gmail.com>
2021-03-25 13:36:16 +1100

    card template

---
+++ src/components/CardDisplay/Card.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+// import ReactDOM from 'react-dom';
+import 'antd/dist/antd.css';
+import { Col, Row, Card } from 'antd';
+
+import contentStyle from './CardDisplay.module.less';
+
+import data from './data'
+
+const { Meta } = Card;
+
+function Cards(){
+  return(
+    <div className={contentStyle.card}>
+      <Row gutter={8}>
+       {data.map((card) => (
+         <Col className="gutter-row" span={6}>
+            <Card
+              hoverable
+                cover={<img alt="example" src= {card.url} 
+              />}
+            >
+              <Meta title= {card.title} description= {card.discription} />
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </div>
+  )
+};
+
+export default Cards;
--- src/components/CardDisplay/CardDisplay.jsx
+++ src/components/CardDisplay/CardDisplay.jsx
@@ -1,40 +1,17 @@
 import React from 'react';
 // import ReactDOM from 'react-dom';
 import 'antd/dist/antd.css';
-import { Col, Row, Card } from 'antd';
 
 import contentStyle from './CardDisplay.module.less';
 
-import data from './data'
-
-const { Meta } = Card;
-
-function cards(){
-  return(
-    <div className={contentStyle.card}>
-      <Row gutter={8}>
-       {data.map((card) => (
-         <Col className="gutter-row" span={6}>
-            <Card
-              hoverable
-                cover={<img alt="example" src= {card.url} 
-              />}
-            >
-              <Meta title= {card.title} description= {card.discription} />
-            </Card>
-          </Col>
-        ))}
-      </Row>
-    </div>
-  )
-};
+import Cards from './Card'
 
 class Content extends React.Component {
   render() {
     return (
       <div className={contentStyle.background_container}>
         <div className={contentStyle.container}>
-          {cards()} 
+          <Cards />
         </div>
       </div>
     );
