9cc14614845801b0940bdc8c3aaa9eb2045e4533
SpeedingRoo <speedingroo@gmail.com>
2021-05-01 20:24:44 +1000

    successfully fetched data from the backend and insert them into the table

--- src/components/RedoAndFavTable/RedoAndFavTable.jsx
+++ src/components/RedoAndFavTable/RedoAndFavTable.jsx
@@ -1,65 +1,100 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {
-  Layout, Table, Space, Button,
+  Layout, Table, Space, Button, Modal,
 } from 'antd';
-import data from './fakeData';
+import axios from 'axios';
+// import data from './fakeData';
 import showDeleteConfirm from '../showDeleteConfirm/showDeleteConfirm';
 
 const { Content } = Layout;
 const { Column } = Table;
 
-const RedoAndFavTable = () => (
-  <Content
-    className="site-layout-background"
-    style={{
-      padding: 24,
-      margin: 0,
-      minHeight: 280,
-    }}
-  >
-    <Table dataSource={data}>
-      <Column title="Quiz ID" dataIndex="quizId" key="quizId" />
-      <Column title="Quiz Title" dataIndex="quizTitle" key="quizTitle" />
-      <Column title="Category" dataIndex="category" key="category" />
-      <Column title="Correct Rate" dataIndex="correctRate" key="correctRate" />
-      <Column title="Timestamp" dataIndex="timestamp" key="timestamp" />
-      <Column
-        title="Action"
-        key="action"
-        // Renderer of the table cell. function(text, record, index) {}
-        render={(record) => (
-          <Space size="middle">
-            <Button
-              type="link"
-              onClick={() => {
-                // ***********************************************************
-                // NOTE: You need modify code in this block
-                // ***********************************************************
-                // eslint-disable-next-line
-              console.log(record.quizId);
-              }}
-            >
-              View
-            </Button>
-            <Button
-              type="link"
-              danger
-              onClick={() => {
-                // ***********************************************************
-                // NOTE: You need add code in this block
-                // ***********************************************************
-                // Modal will show up and the quizId will be transferred to
-                // [ showDeleteConfirm component ]
-                showDeleteConfirm(record.quizId);
-              }}
-            >
-              Delete
-            </Button>
-          </Space>
-        )}
-      />
-    </Table>
-  </Content>
-);
+const RedoAndFavTable = () => {
+  const [dataState, setDataState] = useState();
+  const [modalVisibility, setModalVisibility] = useState(false);
+  const [currentViewedQuizId, setCurrentViewedQuizId] = useState(null);
+  const toggleModalVisibility = () => {
+    setModalVisibility(!modalVisibility);
+  };
+  useEffect(() => {
+    const acquireData = async () => {
+      await axios.post('/redo', {
+        id: localStorage.userId,
+      })
+        .then((response) => {
+          setDataState(response.data);
+          console.log(response);
+        })
+        .catch((error) => {
+          if (error.response) {
+            console.log(error.response);
+          }
+        });
+    };
+    acquireData();
+  }, []);
+
+  return (
+    <Content
+      className="site-layout-background"
+      style={{
+        padding: 24,
+        margin: 0,
+        minHeight: 280,
+      }}
+    >
+      <Table dataSource={dataState}>
+        <Column title="Quiz ID" dataIndex="questionId" key="quizId" />
+        <Column title="Quiz Title" dataIndex="title" key="quizTitle" />
+        <Column title="Category" dataIndex="category" key="category" />
+        <Column
+          title="Action"
+          key="action"
+          // Renderer of the table cell. function(text, record, index) {}
+          render={(record) => (
+            <Space size="small">
+              <Button
+                type="link"
+                onClick={() => {
+                  // ***********************************************************
+                  // NOTE: You need modify code in this block
+                  // ***********************************************************
+                  // eslint-disable-next-line
+                setCurrentViewedQuizId(record.quizId);
+                  toggleModalVisibility();
+                  console.log(currentViewedQuizId);
+                }}
+              >
+                View
+              </Button>
+              <Button
+                type="link"
+                danger
+                onClick={() => {
+                  // ***********************************************************
+                  // NOTE: You need add code in this block
+                  // ***********************************************************
+                  // Modal will show up and the quizId will be transferred to
+                  // [ showDeleteConfirm component ]
+                  showDeleteConfirm(record.data.quizId);
+                }}
+              >
+                Delete
+              </Button>
+            </Space>
+          )}
+        />
+      </Table>
+      <Modal title="my info modal" visible={modalVisibility} onOk={toggleModalVisibility} onCancel={toggleModalVisibility}>
+        <p>
+          The quiz id is :
+          {
+          currentViewedQuizId
+          }
+        </p>
+      </Modal>
+    </Content>
+  );
+};
 
 export default RedoAndFavTable;
